1f1f4f81bf4119d080602c818e74cb3b343f832f
Adam <adam882200@gmail.com>
2024-06-19 17:47:56 +0100

    Added File Versioning

--- Headers/Version.hpp
+++ Headers/Version.hpp
@@ -1,4 +1,8 @@
 #pragma once
 
-inline const char* NCGI_VERSION = "3.0.0";
-inline const char* NCGI_BRANCH = "GitVersionSync";
+#define NCGI_VERSION_MAJOR 3
+#define NCGI_VERSION_MINOR 0
+#define NCGI_VERSION_PATCH 0
+
+#define NCGI_VERSION "3.0.0"
+#define NCGI_BRANCH "GitVersionSync"
